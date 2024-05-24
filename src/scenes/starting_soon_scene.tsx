9bd5456810027ab780d2e51e8af442bf565e62b8
Arif M <arifdata7@gmail.com>
2024-05-24 17:59:45 +0700

    deleted the star png

--- src/scenes/starting_soon_scene.tsx
+++ src/scenes/starting_soon_scene.tsx
@@ -3,22 +3,21 @@ import {all, createRef} from '@motion-canvas/core';
 
 export default makeScene2D(function* (view) {
   // Create your animations here
-  view.fill('#091e26'); //background color
+  view.fill('#3C486B'); //background color
 
-  const circle = createRef<Circle>();
   const userName = createRef<Txt>();
   const startingSoonText = createRef<Txt>();
+  const centerCircle = createRef<Circle>();
 
-  view.add(<Circle ref={circle} size={100} fill={'#0000ff'} />);
-  view.add(<Txt ref={userName} text={"ArifM Streaming"} fontFamily={'Poetsen One'} fill={'#FFF7FC'} fontSize={72} y={-300} />);
-  view.add(<Txt ref={startingSoonText} text={"Starting Soon..."} fontFamily={'Poetsen One'} fill={'#FFF7FC'} fontSize={60} y={-200} />);
+  view.add(<Txt ref={userName} text={"ArifM Streaming"} fontFamily={'Poetsen One'} fill={'#F0F0F0'} fontSize={80} y={-300} />);
+  view.add(<Txt ref={startingSoonText} text={"Starting Soon..."} fontFamily={'Poetsen One'} fill={'#F0F0F0'} fontSize={72} y={-150} />);
   view.add(
     <Line
       points={[
         [-683, 0],
         [-200, 0],
       ]}
-      stroke={'#FFF7FC'}
+      stroke={'#F0F0F0'}
       lineWidth={15}
       radius={40}
     />,
@@ -29,14 +28,24 @@ export default makeScene2D(function* (view) {
         [200, 0],
         [680, 0],
       ]}
-      stroke={'#FFF7FC'}
+      stroke={'#F0F0F0'}
       lineWidth={15}
       radius={40}
     />,
   );
+  view.add(
+    <Circle
+      ref={centerCircle}
+      size={100}
+      x={-130}
+      fill={'#F9D949'}
+    />,
+  );
 
   yield* all(
-    userName().scale(1.1, 2).to(1, 2),
-    startingSoonText().scale(0.8, 2).to(1, 2)
+    //userName().scale(1.1, 2).to(1, 2),
+    //startingSoonText().scale(0.9, 2).to(1, 2),
+    centerCircle().position.x(130, 2).to(-130, 2),
+    centerCircle().fill('#F45050', 2).to('#F9D949', 2),
   );
 });
