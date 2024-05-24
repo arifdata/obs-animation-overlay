2cbd3311ed64100f29f008e9287f2a10a98f5c81
Arif M <arifdata7@gmail.com>
2024-05-24 17:03:26 +0700

    added centered line

--- src/scenes/starting_soon_scene.tsx
+++ src/scenes/starting_soon_scene.tsx
@@ -1,5 +1,5 @@
-import {Circle, Txt, makeScene2D} from '@motion-canvas/2d';
-import {createRef} from '@motion-canvas/core';
+import {Circle, Txt, Line, makeScene2D} from '@motion-canvas/2d';
+import {all, createRef} from '@motion-canvas/core';
 
 export default makeScene2D(function* (view) {
   // Create your animations here
@@ -12,6 +12,31 @@ export default makeScene2D(function* (view) {
   view.add(<Circle ref={circle} size={100} fill={'#0000ff'} />);
   view.add(<Txt ref={userName} text={"ArifM Streaming"} fontFamily={'Poetsen One'} fill={'#FFF7FC'} fontSize={72} y={-300} />);
   view.add(<Txt ref={startingSoonText} text={"Starting Soon..."} fontFamily={'Poetsen One'} fill={'#FFF7FC'} fontSize={60} y={-200} />);
+  view.add(
+    <Line
+      points={[
+        [-683, 0],
+        [-200, 0],
+      ]}
+      stroke={'#FFF7FC'}
+      lineWidth={15}
+      radius={40}
+    />,
+  );
+  view.add(
+    <Line
+      points={[
+        [200, 0],
+        [680, 0],
+      ]}
+      stroke={'#FFF7FC'}
+      lineWidth={15}
+      radius={40}
+    />,
+  );
 
-  yield* circle().scale(2, 2).to(1, 2);
+  yield* all(
+    userName().scale(1.1, 2).to(1, 2),
+    startingSoonText().scale(0.8, 2).to(1, 2)
+  );
 });
