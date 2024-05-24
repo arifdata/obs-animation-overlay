9fc60a0e0d64eb4221ebd762bd126a363934a65e
Arif M <arifdata7@gmail.com>
2024-05-25 06:01:36 +0700

    duplicate starting soon scene into: be right back & stream is ending scene

--- src/project.ts
+++ src/project.ts
@@ -1,10 +1,13 @@
 import {makeProject} from '@motion-canvas/core';
 
 // import example from './scenes/example?scene'; //example scene
-import starting_soon from './scenes/starting_soon_scene?scene';
+//import starting_soon from './scenes/starting_soon_scene?scene';
+// import be_right_back from './scenes/be_right_back?scene';
+import stream_is_ending from './scenes/stream_is_ending?scene';
 
 import './global.css';
 
 export default makeProject({
-  scenes: [starting_soon],
+  //scenes: [starting_soon],
+  scenes: [stream_is_ending],
 });
