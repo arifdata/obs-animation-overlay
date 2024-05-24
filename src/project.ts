0b1ae1f1ef2731348b3cf7924b16453d49d3d6af
Arif M <arifdata7@gmail.com>
2024-05-24 16:36:49 +0700

    added custom font

--- src/project.ts
+++ src/project.ts
@@ -3,6 +3,8 @@ import {makeProject} from '@motion-canvas/core';
 // import example from './scenes/example?scene'; //example scene
 import starting_soon from './scenes/starting_soon_scene?scene';
 
+import './global.css';
+
 export default makeProject({
   scenes: [starting_soon],
 });
--- src/scenes/starting_soon_scene.tsx
+++ src/scenes/starting_soon_scene.tsx
@@ -1,4 +1,4 @@
-import {Circle, makeScene2D} from '@motion-canvas/2d';
+import {Circle, Txt, makeScene2D} from '@motion-canvas/2d';
 import {createRef} from '@motion-canvas/core';
 
 export default makeScene2D(function* (view) {
@@ -6,8 +6,12 @@ export default makeScene2D(function* (view) {
   view.fill('#091e26'); //background color
 
   const circle = createRef<Circle>();
+  const userName = createRef<Txt>();
+  const startingSoonText = createRef<Txt>();
 
   view.add(<Circle ref={circle} size={100} fill={'#0000ff'} />);
+  view.add(<Txt ref={userName} text={"ArifM Streaming"} fontFamily={'Poetsen One'} fill={'#FFF7FC'} fontSize={72} y={-300} />);
+  view.add(<Txt ref={startingSoonText} text={"Starting Soon..."} fontFamily={'Poetsen One'} fill={'#FFF7FC'} fontSize={60} y={-200} />);
 
   yield* circle().scale(2, 2).to(1, 2);
 });
