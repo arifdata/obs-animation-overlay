34fd6473675f50d7dbb2e9602f551d307030366e
Arif M <arifdata7@gmail.com>
2024-05-26 07:57:43 +0700

    working on version 2

--- src/scenes/v2/starting_soon_scene.tsx
+++ src/scenes/v2/starting_soon_scene.tsx
@@ -1,29 +1,37 @@
-import {Circle, Txt, Line, Path, makeScene2D} from '@motion-canvas/2d';
-import {all, createRef} from '@motion-canvas/core';
-import {githubLogo, instagramLogo, tiktokLogo, youtubeLogo} from '../../utils/logo_path';
+import {Img, Line, Txt, Rect, Icon, makeScene2D} from '@motion-canvas/2d';
+import {all, createRef, range, makeRef} from '@motion-canvas/core';
+import bg from '../../assets/bg.jpg';
 
 export default makeScene2D(function* (view) {
-  // Create your animations here
-  view.fill('#3C486B'); //background color
+  const rects: Rect[] = [];
+  //background
+  view.add(<Img src={bg} />);
+
+    // Create some rects
+  view.add(
+    range(5).map(i => (
+      <Rect
+        ref={makeRef(rects, i)}
+        width={30}
+        height={30}
+        x={-100 + 50 * i}
+        fill="#F0F0F0"
+        radius={10}
+      />
+    )),
+  );
 
   const userName = createRef<Txt>();
   const startingSoonText = createRef<Txt>();
-  const centerCircle = createRef<Circle>();
-  
-  view.add(<Txt ref={userName} text={"ArifM Streaming"} fontFamily={'Fjalla One'} fill={'#F0F0F0'} fontSize={80} y={-300} />);
-  view.add(<Txt ref={startingSoonText} text={"Starting Soosn..."} fontFamily={'Jaro'} fill={'#F0F0F0'} fontSize={72} y={-150} />);
-  view.add(<Txt text={"@arifmstreaming"} fontFamily={'Poetsen One'} fill={'#F0F0F0'} fontSize={32} y={230} x={-500} />);
-  //view.add(<Txt text={"github.com"} fontFamily={'Poetsen One'} fill={'#F0F0F0'} fontSize={24} y={230} x={-150} />);
-  view.add(<Txt text={"cosmic.cetacea"} fontFamily={'Jaro'} fill={'#F0F0F0'} fontSize={32} y={230} x={170} />);
-  //view.add(<Txt text={"@arifmstreaming"} fontFamily={'Poetsen One'} fill={'#F0F0F0'} fontSize={24} y={230} x={-500} />);
+
   view.add(
     <Line
       points={[
-        [-683, 0],
+        [-680, 0],
         [-200, 0],
       ]}
       stroke={'#F0F0F0'}
-      lineWidth={15}
+      lineWidth={10}
       radius={40}
     />,
   );
@@ -35,64 +43,55 @@ export default makeScene2D(function* (view) {
         [680, 0],
       ]}
       stroke={'#F0F0F0'}
-      lineWidth={15}
+      lineWidth={10}
       radius={40}
     />,
   );
-
+  
+  view.add(<Txt ref={userName} text={"ArifM Streaming"} fontFamily={'Fjalla One'} fill={'#F0F0F0'} fontSize={96} y={-250} />);
+  view.add(<Txt ref={startingSoonText} text={"Starting Soon"} fontFamily={'Fjalla One'} fill={'#F0F0F0'} fontSize={72} y={-120} />);
+  //view.add(<Txt text={"@arifmstreaming"} fontFamily={'Poetsen One'} fill={'#F0F0F0'} fontSize={32} y={230} x={-500} />);
+  //view.add(<Txt text={"github.com"} fontFamily={'Poetsen One'} fill={'#F0F0F0'} fontSize={24} y={230} x={-150} />);
+  //view.add(<Txt text={"cosmic.cetacea"} fontFamily={'Jaro'} fill={'#F0F0F0'} fontSize={32} y={230} x={170} />);
+  //view.add(<Txt text={"@arifmstreaming"} fontFamily={'Poetsen One'} fill={'#F0F0F0'} fontSize={24} y={230} x={-500} />);
   view.add(
-    <Circle
-      ref={centerCircle}
-      size={100}
-      x={-130}
-      fill={'#F9D949'}
-    />,
+    <Icon
+    y={150}
+    x={-550}
+    icon={'bxl:youtube'}
+    size={[100, 100]}
+     />
   );
 
   view.add(
-    <Path
-      scale={0.2}
-      fill={'#F0F0F0'}
-      data={youtubeLogo}
-      y={100}
-      x={-550}      
-    />,
+    <Icon
+    y={150}
+    x={-200}
+    icon={'bxl:github'}
+    size={[100, 100]}
+     />
   );
 
   view.add(
-    <Path
-      scale={0.2}
-      fill={'#F0F0F0'}
-      data={githubLogo}
-      y={100}
-      x={-200}      
-    />,
+    <Icon
+    y={150}
+    x={130}
+    icon={'bxl:instagram'}
+    size={[100, 100]}
+     />
   );
 
   view.add(
-    <Path
-      scale={0.2}
-      fill={'#F0F0F0'}
-      data={instagramLogo}
-      y={100}
-      x={130}      
-    />,
-  );
-
-  view.add(
-    <Path
-      scale={0.2}
-      fill={'#F0F0F0'}
-      data={tiktokLogo}
-      y={100}
-      x={450}      
-    />,
+    <Icon
+    y={150}
+    x={450}
+    icon={'bxl:tiktok'}
+    size={[100, 100]}
+     />
   );
 
   yield* all(
     //userName().scale(1.1, 2).to(1, 2),
-    //startingSoonText().scale(0.9, 2).to(1, 2),
-    centerCircle().position.x(130, 2).to(-130, 2),
-    centerCircle().fill('#F45050', 2).to('#F9D949', 2),
+    startingSoonText().fill('#ff0000', 2),
   );
 });
