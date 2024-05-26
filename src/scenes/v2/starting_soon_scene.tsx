d7d0eb530b86e6fc3df62283bdde772555ef6435
Arif M <arifdata7@gmail.com>
2024-05-26 11:00:10 +0700

    playing with for loop animation

--- src/scenes/v2/starting_soon_scene.tsx
+++ src/scenes/v2/starting_soon_scene.tsx
@@ -1,5 +1,5 @@
 import {Img, Line, Txt, Rect, Icon, makeScene2D} from '@motion-canvas/2d';
-import {all, createRef, range, makeRef} from '@motion-canvas/core';
+import {all, createRef, range, makeRef, sequence} from '@motion-canvas/core';
 import bg from '../../assets/bg.jpg';
 
 export default makeScene2D(function* (view) {
@@ -14,9 +14,9 @@ export default makeScene2D(function* (view) {
         ref={makeRef(rects, i)}
         width={30}
         height={30}
-        x={-100 + 50 * i}
-        fill="#F0F0F0"
-        radius={10}
+        x={-130 + 65 * i}
+        fill="#FFA732"
+        radius={30}
       />
     )),
   );
@@ -48,16 +48,16 @@ export default makeScene2D(function* (view) {
     />,
   );
   
-  view.add(<Txt ref={userName} text={"ArifM Streaming"} fontFamily={'Fjalla One'} fill={'#F0F0F0'} fontSize={96} y={-250} />);
-  view.add(<Txt ref={startingSoonText} text={"Starting Soon"} fontFamily={'Fjalla One'} fill={'#F0F0F0'} fontSize={72} y={-120} />);
+  view.add(<Txt ref={userName} text={"ArifM Streaming"} fontFamily={'Anton'} fill={'#F0F0F0'} fontSize={96} y={-250} />);
+  view.add(<Txt ref={startingSoonText} text={"Starting Soon"} fontFamily={'Anton'} fill={'#F0F0F0'} fontSize={72} y={-120} />);
   //view.add(<Txt text={"@arifmstreaming"} fontFamily={'Poetsen One'} fill={'#F0F0F0'} fontSize={32} y={230} x={-500} />);
   //view.add(<Txt text={"github.com"} fontFamily={'Poetsen One'} fill={'#F0F0F0'} fontSize={24} y={230} x={-150} />);
   //view.add(<Txt text={"cosmic.cetacea"} fontFamily={'Jaro'} fill={'#F0F0F0'} fontSize={32} y={230} x={170} />);
   //view.add(<Txt text={"@arifmstreaming"} fontFamily={'Poetsen One'} fill={'#F0F0F0'} fontSize={24} y={230} x={-500} />);
   view.add(
     <Icon
-    y={150}
-    x={-550}
+    y={160}
+    x={-520}
     icon={'bxl:youtube'}
     size={[100, 100]}
      />
@@ -65,8 +65,8 @@ export default makeScene2D(function* (view) {
 
   view.add(
     <Icon
-    y={150}
-    x={-200}
+    y={160}
+    x={-175}
     icon={'bxl:github'}
     size={[100, 100]}
      />
@@ -74,8 +74,8 @@ export default makeScene2D(function* (view) {
 
   view.add(
     <Icon
-    y={150}
-    x={130}
+    y={160}
+    x={175}
     icon={'bxl:instagram'}
     size={[100, 100]}
      />
@@ -83,15 +83,19 @@ export default makeScene2D(function* (view) {
 
   view.add(
     <Icon
-    y={150}
-    x={450}
+    y={160}
+    x={500}
     icon={'bxl:tiktok'}
     size={[100, 100]}
      />
   );
 
+    // Animate them
   yield* all(
-    //userName().scale(1.1, 2).to(1, 2),
-    startingSoonText().fill('#ff0000', 2),
+    sequence(0.5, ...rects.map(rect => rect.position.y(50, 1).to(-50, 1).to(0, 1)),),
+    all(...rects.map(rect => rect.fill('#FFA732', 0.5).to('#EF4040', 2).to('#FFA732', 0.5),)),
+    all(...rects.map(rect => rect.radius(30, 0.5).to(10, 2).to(30, 0.5),)),
   );
+  //yield* sequence(0.1, ...rects.map(rect => rect.position.y(50, 1).to(-50, 1).to(0, 1)),);
+
 });
